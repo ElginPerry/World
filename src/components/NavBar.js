6db72b462426ab3d649f7756d6b7b32b52f67263
ElginPerry <elginPerry@yahoo.com>
2020-05-18 18:29:04 -0400

    Added Logout

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,7 @@ import {useSelector} from 'react-redux'
 import "../styles/stylesheet.css"
 
 function NavBar() {
+    const UserEmail = useSelector(state => state.user.UserEmail);
     return (
         <div>
             <nav>
@@ -17,7 +18,12 @@ function NavBar() {
                             <li><a href="/GalaxyView" title="Sub Menu">Galaxy</a></li>
                             <li><a href="/SectorView" title="Sub Menu">Sector</a></li>
                             <li><a href="/SystemView" title="Sub Menu">System</a></li>
+                            {UserEmail &&
+                            <li><a href="/Logout/lo" title="Sub Menu">Logout</a></li>
+                            }
+                            {!UserEmail &&
                             <li><a href="/" title="Sub Menu">Login</a></li>
+                            }
                         </ul>
                         </li>
                         <li className='has-submenu'><a className='prett' href='#' title='Dropdown 1'>Ships</a>
